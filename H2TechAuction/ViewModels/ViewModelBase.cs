15051936fe1df4825540bfcc8c1e19ce1e12e6e1
Aksel Binderup Kristensen <APT@kmd.dk>
2024-09-24 12:10:33 +0200

    Add project files.

---
+++ H2TechAuction/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace H2TechAuction.ViewModels
+{
+    public class ViewModelBase : ObservableObject
+    {
+    }
+}
---
+++ H2TechAuction/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace H2TechAuction.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
